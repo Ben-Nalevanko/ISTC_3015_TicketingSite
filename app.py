f7c074064858a812729328da43a99551470dbe7f
Ben Nalevanko <ben.nalevanko@gmail.com>
2025-04-24 23:06:31 -0400

    More basic structure to html, css, flask

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ import asyncio
 app = Flask(__name__)
 db = "/some/path/later/"
 
+#temporary until we get a db connection set up
+data = [
+	["Event One Name", "Event One Location"],
+	["Event Two Name", "Event Two Locattion"],
+	["Event Three Name", "Event Three Location"]
+	]
 @app.route("/")
 def index():
-	return render_template("index.html")
+	return render_template("index.html", data = data)
 
 if __name__ == '__main__':
 	app.run(debug=True)
