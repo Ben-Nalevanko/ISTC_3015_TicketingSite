7336341a5707788bb0af69bb9aba1963537d1633
Ben Nalevanko <ben.nalevanko@gmail.com>
2025-04-26 23:18:02 -0400

    Structural stuff for buying tickets

--- static/script/script.js
+++ static/script/script.js
@@ -18,3 +18,36 @@ function loadPurchasePage(event_name){
 		console.error("Error:", error);
 	})
 }
+
+function addTicketDiv(){
+	const ticketDiv = document.getElementById("ticketDiv");
+
+	const newTicket = document.createElement("div");
+	const ticketTbl = document.createElement("table");
+	const tbody = document.createElement("tbody");
+	const row = document.createElement("tr");
+
+	const selectList = document.createElement("select");
+
+	//var options = ["Select Ticket Type...", "Child		(Value)", "Adult		(Value)", "Veteran		(Value)"];
+	//Dummy values
+	var options = {
+		"Child": 7,
+		"Adult": 10,
+		"Veteran": 7,
+	}
+	for (var key in options) {
+		var option = document.createElement("option");
+		option.value = key;
+		option.text = key;
+		selectList.appendChild(option);
+	}
+	ticketTbl.appendChild(selectList);
+	newTicket.appendChild(selectList);
+	ticketDiv.appendChild(newTicket);
+
+}
+function removeTicket(){
+	const ticketDiv = document.getElementById("ticketDiv");
+	ticketDiv.removeChild(ticketDiv.lastElementChild);
+}
